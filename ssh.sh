3172be2f57057c4dc160fb4182a6bf20c306c3ae
ahegaowpeace <arduino0202@gmail.com>
2020-05-09 21:06:02 +0900

    login macro

---
+++ ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Raspberry Pi old
+TAR="【ユーザ名】:【ホスト名】"
+PW="【パスワード】"
+
+expect -c "
+set timeout 5
+spawn env LANG=C /usr/bin/ssh ${TAR}
+expect \"password:\"
+send \"${PW}\n\"
+interact
+"
